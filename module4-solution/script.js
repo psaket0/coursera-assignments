1688fdf67c2180a701f22167a34e85f930b62fe3
psaket0 <pathaksaket0@gmail.com>
2022-03-20 19:17:12 -0700

    empty

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -21,6 +21,9 @@ Hello Paula
 Hello Laura
 Good Bye Jim
 
+
+
+
 WARNING!!! WARNING!!!
 The code does NOT currently work! It is YOUR job to make it work
 as described in the requirements and the steps in order to complete this
